80bfe45e53952370d9884e1b8d8be6062c597d01
Corey Garst <corey.garst@gmail.com>
2020-09-18 21:14:44 -0400

    Add a configuration options, error checking, and bugfixes.

--- gamestream_launchpad.py
+++ gamestream_launchpad.py
@@ -4,6 +4,7 @@ import win32.lib.win32con as win32con
 import win32gui
 import pywintypes
 import pyautogui
+import configparser
 import subprocess
 import psutil
 import os
@@ -14,9 +15,43 @@ from time import sleep
 def windowEnumerationHandler(hwnd, top_windows):
     top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
 
+# Define a default config file to write if we're missing one
+config_filename = 'gamestream_launchpad_cfg.ini'
+default_config = """[LAUNCHER]
+# The path to your Playnite.FullscreenApp.exe
+launcher_path = %%LOCALAPPDATA%%\Playnite\Playnite.FullscreenApp.exe
+
+[BACKGROUND]
+# List as many exe's as you want here. They will run at the start of the GameStream session and be killed at the end.
+background_exe_1 = C:\Program Files (x86)\JoyToKey\JoyToKey.exe
+# background_exe_2 = C:\WINDOWS\system32\mspaint.exe
+
+[SETTINGS]
+# Set debug = 1 to leave a window running after gamestream to see error messages from GSLP
+debug = 0
+"""
+
+# Write the default config
+if not os.path.exists(config_filename):
+    with open(config_filename, 'w') as out_file:
+        out_file.write(default_config)
+
+# Parse the config file
+config = configparser.ConfigParser()
+config.read(config_filename)
+cfg_launcher = config['LAUNCHER'].get('launcher_path', r'%LOCALAPPDATA%\Playnite\Playnite.FullsreenApp.exe')
+cfg_bg_paths = config['BACKGROUND']
+cfg_settings = config['SETTINGS']
+debug = cfg_settings.get('debug', '0')
+
 # Target resolution for gamestream environment
-gamestream_width = sys.argv[1]
-gamestream_height = sys.argv[2]
+try:
+    gamestream_width = sys.argv[1]
+    gamestream_height = sys.argv[2]
+except IndexError:
+    print("Error parsing host resolution arguments. Did you mean to run one of the .bat launcher scripts?")
+    print("Usage: gamestream_launchpad.exe 1920 1080")
+    sys.exit(1)
 
 # Set resolution to target
 print("Switching resolution to {0}x{1}".format(gamestream_width, gamestream_height))
@@ -30,19 +65,34 @@ win32api.ChangeDisplaySettings(devmode, 0)
 print("Minimizing windows")
 pyautogui.hotkey('winleft', 'd')
 
-# Start JoyToKey if installed
-j2k_path = 'C:\Program Files (x86)\JoyToKey\JoyToKey.exe'
-if os.path.exists(j2k_path):
-    print("Launching JoyToKey")
-    j2k = True
-    if "JoyToKey.exe" in (p.name() for p in psutil.process_iter()):
-        os.system('taskkill /f /im JoyToKey.exe')
-    subprocess.Popen(j2k_path)
+# Start background programs, if they're available
+for path in cfg_bg_paths:
+    expanded_path = os.path.expandvars(cfg_bg_paths[path])
+    if os.path.exists(expanded_path):
+        print("Launching", expanded_path)
+        exec_name = os.path.basename(expanded_path)
+        # Kill the process first if it's already running
+        if exec_name in (p.name() for p in psutil.process_iter()):
+            os.system('taskkill /f /im ' + exec_name)
+        # Start the process
+        subprocess.Popen(expanded_path)
+
+# Kill leftover game launchers
+launcher_exec_name = os.path.basename(cfg_launcher)
+if launcher_exec_name in (p.name() for p in psutil.process_iter()):
+    os.system('taskkill /f /im ' + launcher_exec_name)
+
+# Specific case for alternate versions of Playnite
+if "Playnite" in launcher_exec_name:
+    if "Playnite.FullscreenApp.exe" in (p.name() for p in psutil.process_iter()):
+        os.system('taskkill /f /im ' + "Playnite.FullscreenApp.exe")
+    if "Playnite.DesktopApp.exe" in (p.name() for p in psutil.process_iter()):
+        os.system('taskkill /f /im ' + "Playnite.DesktopApp.exe")
 
 # Start game launcher
-print("Launching Playnite")
-playnite_exe = os.path.join(os.getenv('LOCALAPPDATA'), 'Playnite', 'Playnite.FullscreenApp.exe')
-subprocess.Popen(playnite_exe)
+print("Starting game launcher")
+launcher_exe = os.path.expandvars(cfg_launcher)
+subprocess.Popen(launcher_exe)
 
 # Move mouse cursor into the lower-right corner to pseudo-hide it
 pyautogui.FAILSAFE = False
@@ -71,11 +121,14 @@ while True:
     else:
         break
 
-# Kill joy2key
-if j2k:
-    if "JoyToKey.exe" in (p.name() for p in psutil.process_iter()):
-        print("Terminating joy2key.")
-        os.system('taskkill /f /im JoyToKey.exe')
+# Terminate background programs, if they're available
+for path in cfg_bg_paths:
+    expanded_path = os.path.expandvars(cfg_bg_paths[path])
+    if os.path.exists(expanded_path):
+        exec_name = os.path.basename(expanded_path)
+        print("Terminating", exec_name)
+        if exec_name in (p.name() for p in psutil.process_iter()):
+            os.system('taskkill /f /im ' + exec_name)
 
 # Kill gamestream
 print("Terminating GameStream session.")
@@ -85,3 +138,6 @@ if "nvstreamer.exe" in (p.name() for p in psutil.process_iter()):
 # Restore original resolution
 print('Restoring original resolution.')
 win32api.ChangeDisplaySettings(None, 0)
+
+if debug == '1':
+    input("Paused for debug review. Press Enter key to close.")
